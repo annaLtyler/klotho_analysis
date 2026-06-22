e129c97e87f7a1ff2f8dc92cb62298531711bb55
annaLtyler <48364995+annaLtyler@users.noreply.github.com>
2026-06-22 12:32:21 -0400

    updates

--- Code/qqunif.plot.R
+++ Code/qqunif.plot.R
@@ -2,13 +2,20 @@
 qqunif.plot<-function(pvalues, plot.label = "P Value qq plot",
 xlab=expression(paste("Expected (",-log[10], " p-value)")),
 ylab=expression(paste("Observed (",-log[10], " p-value)")),
-line.col = "black"){
+line.col = "black", label.above = NULL, label.pos = 4, 
+label.cex = 1, xlim = NULL){
 	
 	exp.dist <- runif(10000)
 	log.exp <- -log10(exp.dist)
 	log.obs <- -log10(pvalues)
 
-	qqplot(log.exp, log.obs, xlab = xlab, ylab = ylab, main = plot.label)
+	coords <- qqplot(log.exp, log.obs, xlab = xlab, ylab = ylab, main = plot.label, xlim = xlim)
 	abline(0,1, col = line.col)
+
+	if(!is.null(label.above)){
+		label.idx <- which(coords$y >= label.above)
+		text(coords$x[label.idx], coords$y[label.idx], labels = names(label.idx), 
+			pos = label.pos, cex = label.cex)
+	}
 
 }
